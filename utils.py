ea314d782d8c55b5c6effd7833e9bf2b4891a6bd
TheNizzo <khalili-nizar@hotmail.com>
2021-10-05 21:36:57 +0200

    added typing to utils

--- utils.py
+++ utils.py
@@ -1,14 +1,17 @@
 from nltk.stem.snowball import SnowballStemmer
 from nltk.tokenize import word_tokenize
+from typing import List
+from typing import Dict
 from tqdm import tqdm
 import numpy as np
+import pandas as pd
 import spacy
 import nltk
 import re
 nltk.download('punkt')
 nlp = spacy.load("en_core_web_sm", disable=["parser", "ner"])
 
-def stem(l):
+def stem(l: List[str]) -> List[str]:
   """
   Takes in a list of phrases l, applys stemming, returns the stemmed list.
   """
@@ -19,7 +22,7 @@ def stem(l):
     res.append(" ".join([stemmer.stem(word) for word in word_tokenize(text.lower()) if re_word.match(word)]))
   return res
 
-def lemm(l):
+def lemm(l: List[str]) -> List[str]:
   """
   Takes in a list of phrases l, applys lemmatization, returns the lemmatized list.
   """
@@ -30,7 +33,7 @@ def lemm(l):
   return lemmas
 
 
-def pos(l, d):
+def pos(l: List[str], d: Dict[str, float]) -> int:
   """
   Takes a list of words l, and a dictionnary of rated words d, returns the number of positive words in the list.
   """
@@ -40,7 +43,7 @@ def pos(l, d):
       pos += 1
   return pos
 
-def neg(l, d):
+def neg(l: List[str], d: Dict[str, float]) -> int:
   """
   Takes a list of words l, and a dictionnary of rated words d, returns the number of negative words in the list.
   """
@@ -50,13 +53,13 @@ def neg(l, d):
       neg += 1
   return neg
 
-def contains_no(l):
+def contains_no(l: List[str]) -> bool:
   """
   takes in a list of words l, returns True if list contains the word "no" else False.
   """
   return 1 if ("no" in list((map(lambda x: x.lower(),l)))) else 0
 
-def first_second_pro(l):
+def first_second_pro(l: List[str]) -> int:
   """
   takes in a list of words l, returns count of first and second pronouns in the list.
   """
@@ -64,7 +67,7 @@ def first_second_pro(l):
               "yours"]
   return sum([list((map(lambda x: x.lower(),l))).count(j) for j in pronouns])
 
-def get_features(df, d):
+def get_features(df: pd.DataFrame, d: Dict[str, float]):
   """
   takes in a dataframe df, and a dictionnary d of rated words, changes the df with added features columns.
   """
